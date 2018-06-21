cefbcdbcdfce4098532712bc1c4342d6e416bf3d
Peter Froud <pfroud@gmail.com>
2018-06-20 23:46:41 -0700

    add countdown timer

---
+++ CountdownTimer.js
@@ -0,0 +1,80 @@
+"use strict";
+
+class CountdownTimer {
+
+    constructor(durationMs) {
+        if (!durationMs) {
+            console.error("duration is required");
+            return;
+        }
+        this.intervalMs = 100;
+        this.durationMs = durationMs;
+        this.remainingMs = durationMs;
+        this.intervalID = null;
+        this.isRunning = false;
+
+        // events
+        this.onStart = null;
+        this.onPause = null;
+        this.onResume = null;
+        this.onReset = null;
+        this.onFinished = null;
+        this.onTick = null;
+
+        // display elements
+        this.textElement = null;
+//        this.progressElement = null;
+//        this.dotsElement = null;
+    }
+
+    pause() {
+        if (this.isRunning) {
+            window.clearInterval(this.intervalID);
+            this.onPause && this.onPause();
+            this.isRunning = false;
+        }
+    }
+
+    resume() {
+        if (!this.isRunning) {
+            this.onResume & this.onResume();
+            this.isRunning = true;
+            this.intervalID = window.setInterval(this._intervalHandler, this.intervalMs);
+        }
+    }
+
+    reset() {
+        this.onReset && this.onReset();
+        this.isRunning = false;
+        this.remainingMs = this.durationMs;
+        window.clearInterval(this.intervalID);
+        this.textElement && this.textElement.html(this.remainingMs + ".0");
+    }
+
+    start() {
+        if (!this.isRunning) {
+            this.onStart && this.onStart();
+            this.isRunning = true;
+            this.intervalID = window.setInterval(this._intervalHandler, this.intervalMs, this);
+        }
+    }
+
+    _intervalHandler(instance) {
+        instance.remainingMs -= instance.intervalMs;
+
+        instance.onTick && instance.onTick();
+
+        if (instance.textElement) {
+            var secondsRoundedDown = Math.floor(instance.remainingMs / 1000);
+            var tenthSeconds = (instance.remainingMs % 1000) / 100;
+            instance.textElement.html(secondsRoundedDown + "." + tenthSeconds);
+        }
+
+        if (instance.remainingMs <= 0) {
+            instance.isRunning = false;
+            window.clearInterval(instance.intervalID);
+            instance.onFinished && instance.onFinished();
+        }
+    }
+
+}
