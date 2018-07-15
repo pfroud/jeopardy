d3889ff24d228363fc756eac7615a5f140855a26
Peter Froud <pfroud@gmail.com>
2018-07-15 13:46:27 -0700

    add keyboard shortcuts for operator tabs

--- operator/mainOperator.js
+++ operator/mainOperator.js
@@ -1,15 +1,51 @@
 
 
 
+
 const SETTINGS = new Settings();
 
 var operatorInstance = null;
 $(document).ready(function () {
-
-//    operatorInstance = new Operator();
-
+    if (window.location.search.length < 1) {
+        operatorInstance = new Operator();
+    }
+    initKeyboardShortcuts();
 });
 
 function handleDisplayWindowReady() {
     operatorInstance.initTeams();
+}
+
+function initKeyboardShortcuts() {
+    const inputSetup = $("input#tabSetup");
+    const inputOperate = $("input#tabOperate");
+    const inputAdmin = $("input#tabAdmin");
+    const inputSettings = $("input#tabSettings");
+
+    document.addEventListener("keydown", function (keyboardEvent) {
+//        if (!keyboardEvent.altKey) {
+//            return;
+//        }
+
+/*
+ $('input[type="radio"]:checked').attr("id") 
+ */
+
+        switch (keyboardEvent.key.toLowerCase()) {
+            case "s":
+                inputSetup.prop("checked", true);
+                break;
+            case "o":
+                inputOperate.prop("checked", true);
+                break;
+            case "a":
+                inputAdmin.prop("checked", true);
+                break;
+            case "e":
+                inputSettings.prop("checked", true);
+//                keyboardEvent.preventDefault(); //chrome opens its menu
+                break;
+
+        }
+    });
 }
