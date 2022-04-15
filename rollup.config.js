b9c5f164dd0eaf782474641181c309e25f0cb6e8
Peter Froud <pfroud@gmail.com>
2022-04-14 18:13:27 -0700

    add rollup.config.js

---
+++ rollup.config.js
@@ -0,0 +1,16 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+
+
+/*
+https://github.com/rollup/plugins/tree/master/packages/node-resolve
+https://rollupjs.org/guide/en/#warning-treating-module-as-external-dependency
+*/
+
+export default {
+  input: 'public_html/chartjs_test.js',
+  output: {
+    file: 'public_html/rollup_bundle.js',
+    format: 'es'
+  },
+  plugins: [nodeResolve()]
+};
