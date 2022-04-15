57022566e08913c62bb6710ca87d8cad511b8590
Peter Froud <pfroud@gmail.com>
2022-04-14 22:17:27 -0700

    use chartist instead of chart.js

---
+++ public_html/chartist-test/chartist_test.ts
@@ -0,0 +1,26 @@
+import * as Chartist from "chartist";
+
+// https://gionkunz.github.io/chartist-js/examples.html
+// https://gionkunz.github.io/chartist-js/api-documentation.html
+
+const data: Chartist.IChartistData = {
+    series: [5, 3, 4],
+    labels: ["hello", "world", "asdf"]
+};
+
+const options: Chartist.IPieChartOptions = {
+    width: "100%",
+    height: "100%",
+    donut: true,
+    donutWidth: "50%",
+    chartPadding: 0,
+    showLabel: true,
+    labelPosition: "outside",
+};
+
+new Chartist.Pie('.ct-chart',
+    data,
+    options
+);
+
+console.log("asdf");
