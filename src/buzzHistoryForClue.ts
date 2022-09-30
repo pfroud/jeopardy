3987ea5c3d44c4b1136f86493c46d6e81d93514a
Peter Froud <pfroud@gmail.com>
2022-09-29 19:38:03 -0700

    Got basic buzz history chart working

--- src/buzzHistoryForClue.ts
+++ src/buzzHistoryForClue.ts
@@ -1,3 +1,4 @@
+import * as d3 from "d3";
 import { Clue } from "./operator/Operator";
 
 export interface BuzzHistoryForClue {
@@ -44,6 +45,83 @@ export const exampleBuzzHistory: BuzzHistoryForClue = {
     timestampWhenClueQuestionFinishedReading: 1664501696893
 };
 
-export function createDiagram(target: HTMLDivElement, history: BuzzHistoryForClue): void {
-    console.log("create diagram");
+export function createDiagram(_svg_: SVGSVGElement, history: BuzzHistoryForClue): void {
+    const svgWidth = 800;
+    const svgHeight = 600;
+    const margin = 20;
+
+    const contentWidth = svgWidth - margin - margin;
+    const contentHeight = svgHeight - margin - margin;
+
+    const xAxisSpaceMillisec = 2000;
+    const timestampDoneReading = history.timestampWhenClueQuestionFinishedReading;
+
+    const rowHeight = 50;
+    const dotRadius = 5;
+
+    const svg = d3.select(_svg_)
+        .attr("width", svgWidth)
+        .attr("height", svgHeight);
+
+    const groupContent = svg.append("g")
+        .attr("id", "content")
+        .attr("transform", `translate(${margin}, ${margin})`);
+
+    const groupAxis = groupContent.append("g")
+        .attr("id", "axis")
+        .attr("transform", `translate(0, ${contentHeight})`);
+
+    const scale = d3.scaleLinear()
+        .domain([timestampDoneReading - xAxisSpaceMillisec, timestampDoneReading + xAxisSpaceMillisec])
+        .range([0, svgWidth - margin - margin]);
+
+    const axis = d3.axisBottom<number>(scale)
+        .ticks(5)
+        .tickSizeOuter(0)
+        .tickFormat(n => (n - timestampDoneReading) + "ms")
+        ;
+
+    groupAxis.call(axis);
+
+    const rowsGroup = groupContent.append("g")
+        .attr("id", "rows");
+
+    const rowsArray: d3.Selection<SVGGElement, unknown, null, undefined>[] = [];
+    for (let i = 0; i < 5; i++) {
+        const group = rowsArray[i] = rowsGroup.append("g")
+            .attr("id", `row-${i}`)
+            .attr("transform", `translate(0, ${rowHeight * i})`);
+
+        group.append("rect")
+            .attr("x", 0)
+            .attr("y", 0)
+            .attr("width", contentWidth)
+            .attr("height", rowHeight)
+            .attr("fill", d3.schemeAccent[i])
+
+        group.append("text")
+            .attr("x", "10")
+            .attr("y", rowHeight / 2)
+            .text("Team " + i);
+
+    }
+
+    groupContent.append("line")
+        .attr("x1", scale(timestampDoneReading))
+        .attr("y1", 0)
+        .attr("x2", scale(timestampDoneReading))
+        .attr("y2", contentHeight)
+        .attr("stroke", "black")
+        .attr("stroke-width", 1);
+
+    history.records.forEach(record => {
+        rowsArray[record.teamNumber].append("circle")
+            .attr("cx", scale(record.timestamp))
+            .attr("cy", rowHeight / 2)
+            .attr("r", dotRadius)
+            ;
+
+    });
+
+
 }
