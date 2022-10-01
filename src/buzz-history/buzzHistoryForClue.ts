b9eb72632eec23afa4af554dc1069fc87ad60c61
Peter Froud <pfroud@gmail.com>
2022-09-30 19:51:40 -0700

    Show dividing lines instead of colored background

--- src/buzz-history/buzzHistoryForClue.ts
+++ src/buzz-history/buzzHistoryForClue.ts
@@ -50,16 +50,21 @@ export enum BuzzResultEnum {
 export function createDiagram(_svg_: SVGSVGElement, history: BuzzHistoryForClue): void {
     const svgWidth = 800;
     const svgHeight = 600;
-    const margin = 20;
+    const margin = {
+        top: 20,
+        left: 20,
+        right: 20,
+        bottom: 20
+    };
 
-    const contentWidth = svgWidth - margin - margin;
-    const contentHeight = svgHeight - margin - margin;
+    const contentWidth = svgWidth - margin.left - margin.right;
+    const contentHeight = svgHeight - margin.top - margin.bottom;
 
-    const spaceBeforeMillisec = 3000;
-    const spaceAfterMillisec = 500;
+    const spaceBeforeMillisec = 2_000;
+    const spaceAfterMillisec = 3_000;
     const timestampDoneReading = history.timestampWhenClueQuestionFinishedReading;
 
-    const rowHeight = 100;
+    const rowHeight = 50;
     const dotRadius = 5;
     const barHeight = dotRadius * 2;
 
@@ -69,7 +74,7 @@ export function createDiagram(_svg_: SVGSVGElement, history: BuzzHistoryForClue)
 
     const groupContent = svg.append("g")
         .attr("id", "content")
-        .attr("transform", `translate(${margin}, ${margin})`);
+        .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
     const groupAxis = groupContent.append("g")
         .attr("id", "axis")
@@ -77,10 +82,10 @@ export function createDiagram(_svg_: SVGSVGElement, history: BuzzHistoryForClue)
 
     const scale = d3.scaleLinear()
         .domain([timestampDoneReading - spaceBeforeMillisec, timestampDoneReading + spaceAfterMillisec])
-        .range([0, svgWidth - margin - margin]);
+        .range([0, contentWidth]);
 
     const axis = d3.axisBottom<number>(scale)
-        .ticks(5)
+        .ticks(4)
         .tickSizeOuter(0)
         .tickFormat(n => (n - timestampDoneReading) + "ms")
         ;
@@ -91,26 +96,41 @@ export function createDiagram(_svg_: SVGSVGElement, history: BuzzHistoryForClue)
         .attr("id", "rows");
 
     const rowsArray: d3.Selection<SVGGElement, unknown, null, undefined>[] = [];
-    for (let i = 0; i < 5; i++) {
+
+    for (let i = 0; i < 8; i++) {
         const group = rowsArray[i] = rowsGroup.append("g")
             .attr("id", `row-${i}`)
             .attr("transform", `translate(0, ${rowHeight * i})`);
 
-        group.append("rect")
-            .attr("x", 0)
-            .attr("y", 0)
-            .attr("width", contentWidth)
-            .attr("height", rowHeight)
-            .attr("fill", d3.schemeAccent[i])
+        if (i % 2 == 0) {
+            group.append("rect")
+                .attr("x", 0)
+                .attr("y", 0)
+                .attr("width", contentWidth)
+                .attr("height", rowHeight)
+                .attr("fill", "#eee");
+        }
+
+
+        group.append("line")
+            .attr("x1", 0)
+            .attr("y1", rowHeight)
+            .attr("x2", contentWidth)
+            .attr("y2", rowHeight)
+            .attr("stroke", "black")
+            .attr("stroke-width", 1)
+            ;
 
         group.append("text")
             .attr("x", "10")
             .attr("y", rowHeight / 2)
-            .text("Team " + i);
+            .attr("dominant-baseline", "middle")
+            .text(`Team ${i + 1}`);
 
     }
 
     groupContent.append("line")
+        .attr("id", "operator-finished-reading-question")
         .attr("x1", scale(timestampDoneReading))
         .attr("y1", 0)
         .attr("x2", scale(timestampDoneReading))
@@ -119,41 +139,53 @@ export function createDiagram(_svg_: SVGSVGElement, history: BuzzHistoryForClue)
         .attr("stroke-width", 1);
 
     history.records.forEach(record => {
-
-
-        const group = rowsArray[record.teamNumber];
-        group.append("circle")
-            .attr("cx", scale(record.timestamp))
-            .attr("cy", rowHeight / 2)
-            .attr("r", dotRadius)
-            .attr("fill", "#000000aa")
-            .attr("stroke-width", 0);
-
+        const groupForTeam = rowsArray[record.teamNumber];
         const yForBars = (rowHeight / 2) - (barHeight / 2);
         switch (record.result.type) {
-
             case BuzzResultEnum.TOO_EARLY:
-                group.append("rect")
+                groupForTeam.append("rect")
                     .attr("x", scale(record.timestamp))
                     .attr("y", yForBars)
                     .attr("width", scale(history.lockoutDurationMillisec) - scale(0))
                     .attr("height", barHeight)
-                    .attr("fill", "#ff000066")
+                    .attr("fill", "red")
                     .attr("stroke", "black")
                     .attr("stroke-width", 1);
 
                 break;
             case BuzzResultEnum.START_ANSWERING:
-                group.append("rect")
+                groupForTeam.append("rect")
                     .attr("x", scale(record.timestamp))
                     .attr("y", yForBars)
                     .attr("width", scale(record.result.endTimestamp))
                     .attr("height", barHeight)
-                    .attr("fill", record.result.answeredCorrectly ? "green" : "orange")
+                    .attr("fill", "lightblue" /*record.result.answeredCorrectly ? "green" : "orange"*/)
                     .attr("stroke", "black")
                     .attr("stroke-width", 1);
+
+                /*
+                groupContent.append("rect")
+                    .attr("id", "")
+                    .attr("x", scale(record.timestamp))
+                    .attr("y", 0)
+                    .attr("width", scale(record.result.endTimestamp))
+                    .attr("height", contentHeight)
+                    .attr("fill", "#0088ff88")
+                    .attr("stroke", "black")
+                    .attr("stroke-width", 0);
+                */
+
                 break;
         }
+
+        groupForTeam.append("circle")
+            .attr("cx", scale(record.timestamp))
+            .attr("cy", rowHeight / 2)
+            .attr("r", dotRadius)
+            .attr("fill", "black")
+            .attr("stroke-width", 0);
+
+
     });
 
 
