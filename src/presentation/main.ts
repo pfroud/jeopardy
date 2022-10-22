d8d46b4defbd6c575145e0070ce8b57b710e12b3
Peter Froud <pfroud@gmail.com>
2022-10-22 01:38:32 -0700

    Cast window.opener to suppress error TS2339

--- src/presentation/main.ts
+++ src/presentation/main.ts
@@ -9,8 +9,10 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Show errors from the presentation window in the operator window
     window.addEventListener("error", function (errorObj) {
-        window.opener.console.error("ERROR FROM PRESENTATION WINDOW:");
-        window.opener.console.error(errorObj.error);
+        // suppress error TS2339 "property 'console' does not exist on type 'Window'"
+        const castOpener = window.opener as (Window & typeof globalThis);
+        castOpener.console.error("ERROR FROM PRESENTATION WINDOW:");
+        castOpener.console.error(errorObj.error);
     });
 
     // Add global variable so we can access the presentation instance in the web browser debugger.
